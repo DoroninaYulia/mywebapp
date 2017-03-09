a8027db5e92a9febce777071db9cc1a82a577ab8
Yulia <pte08m@gmail.com>
2017-03-09 23:47:44 +0200

    +spring

--- src/main/java/org/itstep/myWebApp/service/UserService.java
+++ src/main/java/org/itstep/myWebApp/service/UserService.java
@@ -2,12 +2,19 @@ package org.itstep.myWebApp.service;
 
 import org.itstep.myWebApp.model.User;
 import org.itstep.myWebApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService {
 
-    private UserRepository repository = new UserRepository();
+    private UserRepository repository/* = new UserRepository()*/;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
 
     public List<User> getAll() {
         return repository.getAll();
--- src/main/java/org/itstep/myWebApp/web/UserServlet.java
+++ src/main/java/org/itstep/myWebApp/web/UserServlet.java
@@ -2,7 +2,11 @@ package org.itstep.myWebApp.web;
 
 import org.itstep.myWebApp.model.User;
 import org.itstep.myWebApp.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+@Controller
 public class UserServlet extends HttpServlet {
+    private UserService service/* = new UserService()*/;
+    private WebApplicationContext context;
 
-    private UserService service = new UserService();
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+        service = context.getBean(UserService.class);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -62,5 +74,6 @@ public class UserServlet extends HttpServlet {
         resp.sendRedirect("users");
 
     }
+
 
 }
